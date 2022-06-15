9af5f71bd5559ae714cfb127d3ea0a62cae7ae66
Sekou Samassa <71543096+sksamassa@users.noreply.github.com>
2022-06-15 08:28:44 +0500

    Update Lucky.java

--- src/Lucky.java
+++ src/Lucky.java
@@ -1,22 +1,20 @@
 package src;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Lucky {
-    private static int x = 0;
-    private static int count = 0;
+    static AtomicInteger x = new AtomicInteger(0);
+    static AtomicInteger count = new AtomicInteger(0);
 
     static class LuckyThread extends Thread {
-        private static final Object lock = new Object();
         @Override
         public void run() {
-            synchronized (lock)
-            {
-                while (x < 999999) {
-                    x++;
-                    if ((x % 10) + (x / 10) % 10 + (x / 100) % 10 == (x / 1000)
-                            % 10 + (x / 10000) % 10 + (x / 100000) % 10) {
-                        System.out.println(x);
-                        count++;
-                    }
+            while (x.get() < 999999) {
+                int x = Lucky.x.incrementAndGet();
+                if ((x % 10) + (x / 10) % 10 + (x / 100) % 10 == (x / 1000)
+                        % 10 + (x / 10000) % 10 + (x / 100000) % 10) {
+                    System.out.println("" + this.getName() + ":: " + x);
+                    count.incrementAndGet();
                 }
             }
         }
@@ -26,12 +24,14 @@ public class Lucky {
         Thread t1 = new LuckyThread();
         Thread t2 = new LuckyThread();
         Thread t3 = new LuckyThread();
+        long start = System.currentTimeMillis();
         t1.start();
         t2.start();
         t3.start();
         t1.join();
         t2.join();
         t3.join();
+        System.out.println("Time: " + (System.currentTimeMillis() - start));
         System.out.println("Total: " + count);
     }
 }
